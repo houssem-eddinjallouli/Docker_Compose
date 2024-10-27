bfeadc7e0651ab1dc3030f02672cf1574aaf5d2d
houssem-eddinjallouli <houssem-eddin.jallouli@esprit.tn>
2024-10-27 07:44:16 +0100

    docker-compose

---
+++ product/api.py
@@ -0,0 +1,22 @@
+# Product Service
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Product(Resource):
+    def get(self):
+        return {
+            'products': [
+                'Product 1',
+                'Product 2',
+                'Product 3',
+                'Product 4']
+            }
+    
+api.add_resource(Product, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
